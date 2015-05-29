b30c32e9ba2253bb07cdac7619ead616bce9fce8
nobita4176 <nobita4176@gmail.com>
2015-05-29 21:11:47 +0900

    vote()の形式の変更

--- js/vote.js
+++ js/vote.js
@@ -10,10 +10,21 @@ requirejs.config({
 require(['Firebase'], function(Firebase) {
 	var firebase = new Firebase('https://analoggamelab-vote.firebaseio.com/');
 
+	var getDateString = function() {
+		var fix = function(num) {return num > 9 ? ''+num : '0'+num;};
+		var date = new Date();
+		return [
+			date.getMonth()+1, '/',
+			date.getDate(), ' ',
+			fix(date.getHours()), ':',
+			fix(date.getMinutes()), ':',
+			fix(date.getSeconds())
+		].join('');
+	};
+
 	// 投票
 	var vote = function(name, value) {
-		var date = new Date();
-		firebase.child(name).set({'value': value, 'date': date});
+		firebase.set({'name': name, 'value': value, 'date': getDateString()});
 	};
 
 	// 投票されたら 表示更新
