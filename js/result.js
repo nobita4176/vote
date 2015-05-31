8dbe9e161e03a0442d0d089c4a7672d5b96c496d
nobita4176 <nobita4176@gmail.com>
2015-06-01 04:21:24 +0900

    投票結果グラフ表示の実装

--- js/result.js
+++ js/result.js
@@ -8,21 +8,11 @@ requirejs.config({
 		'Firebase': {'exports': 'Firebase'}
 	}
 });
+
 require(['Firebase', 'Chart', './util'], function(Firebase, Chart, util) {
 	var chartElement = document.querySelector('#chart');
 	chartElement.setAttribute('width', window.innerWidth);
 	chartElement.setAttribute('height', window.innerHeight);
-
-	/* 
-	// Firebaseインスタンス
-	var firebase = new Firebase('https://analoggamelab-vote.firebaseio.com/');
-	// Firebaseの投票インスタンス
-	var votes = firebase.child('votes');
-
-	// 投票されたら 表示更新
-	votes.on('child_changed', function(childSnapshot, prevChildName) {
-	});
-	*/
 
 	var defineChart = function(targetNames, rawVotes, uniqueVotes) {
 		var data = {
@@ -70,9 +60,61 @@ require(['Firebase', 'Chart', './util'], function(Firebase, Chart, util) {
 		chart.update();
 	};
 
-	var chart = defineChart(
-		['イスタンブール', '海底探検', 'ラブレター', 'スコットランドヤード'],
-		[1, 5, 6, 4],
-		[1, 2, 3, 2]
-	);
+
+	var chart;
+	// Firebaseインスタンス
+	var firebase = new Firebase('https://analoggamelab-vote.firebaseio.com/');
+
+	// 最初にグラフを作る
+	firebase.once('value', function(snapshot) {
+		// 対象の名前のArrayを作る
+		var targetsSS = snapshot.child('targets');
+		var targetNames = [];
+		targetsSS.forEach(function(child) {targetNames.push(child.key());});
+
+		// 投票の集計を取る
+		var votesSS = snapshot.child('votes');
+		var rawVotesMap = {};
+		var uniqueVotesMap = {};
+		targetNames.forEach(function(e) {
+			rawVotesMap[e] = 0;
+			uniqueVotesMap[e] = 0;
+		});
+		votesSS.forEach(function(child) {
+			var value = [
+				child.child('value1').val(),
+				child.child('value2').val(),
+				child.child('value3').val(),
+			];
+			// 普通に加算
+			value.forEach(function(v) {
+				if (v.length === 0) {return;}
+				rawVotesMap[v] += 1;
+			});
+			// 重複を取り除く
+			if (value[2] === value[0] || value[2] === value[1]) {delete value[2];}
+			if (value[1] === value[0]) {delete value[1];}
+			// ユニーク票として加算
+			value.forEach(function(v) {
+				if (v.length === 0) {return;}
+				uniqueVotesMap[v] += 1;
+			});
+		});
+
+		// 投票結果を配列に
+		var rawVotes = new Array(targetNames.length);;
+		var uniqueVotes = new Array(targetNames.length);
+		targetNames.forEach(function(e, i) {
+			rawVotes[i] = rawVotesMap[e];
+			uniqueVotes[i] = uniqueVotesMap[e];
+		});
+
+		// グラフを作る
+		chart = defineChart(targetNames, rawVotes, uniqueVotes);
+	});
+	// 投票されたら 表示更新
+	/*
+	firebase.child('votes').on('child_changed', function(childSnapshot, prevChildName) {
+	});
+	*/
 });
